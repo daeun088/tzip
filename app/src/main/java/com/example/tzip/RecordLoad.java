14cede36ce10f450300c8aac0ed0dd986ce472a9
daeun088 <kde2329@naver.com>
2023-11-22 12:59:42 +0900

    액티비티 수정

---
+++ app/src/main/java/com/example/tzip/RecordLoad.java
@@ -0,0 +1,14 @@
+package com.example.tzip;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class RecordLoad extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_record_load);
+    }
+}
