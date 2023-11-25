82a6d5edc935836f0a8feff55e4effddc353a3fb
daeun088 <kde2329@naver.com>
2023-11-26 04:11:56 +0900

    회원가입 java ing

--- app/src/main/java/com/example/tzip/SignUp.java
+++ app/src/main/java/com/example/tzip/SignUp.java
@@ -2,13 +2,26 @@ package com.example.tzip;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
+import com.example.tzip.databinding.ActivitySignUpBinding;
+
 public class SignUp extends AppCompatActivity {
+    private ActivitySignUpBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_sign_up);
+        binding = ActivitySignUpBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        binding.BackButton.setOnClickListener(v -> {
+
+        });
     }
+
+
+
+
 }
