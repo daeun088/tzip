33163f052760096e34f6287d715c00790c37b6e3
daeun088 <kde2329@naver.com>
2023-11-26 12:33:55 +0900

    회원가입 성공...

---
+++ app/src/main/java/com/example/tzip/FirebaseId.java
@@ -0,0 +1,12 @@
+package com.example.tzip;
+
+public class FirebaseId {
+    public static String user = "user";
+    public static String email = "email";
+    public static String password = "password";
+    public static String documentId = "documentId";
+
+    public static String nickname = "nickname";
+
+
+}
--- app/src/main/java/com/example/tzip/SignUp.java
+++ app/src/main/java/com/example/tzip/SignUp.java
@@ -14,12 +14,17 @@ import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.SetOptions;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class SignUp extends AppCompatActivity {
     private ActivitySignUpBinding binding;
     private FirebaseAuth mAuth;
-
-
+    private FirebaseFirestore userDB = FirebaseFirestore.getInstance();
+    String nickname;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,9 +32,19 @@ public class SignUp extends AppCompatActivity {
         binding = ActivitySignUpBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
+        initFirebaseAuth();
+
         binding.signupButton.setOnClickListener( v-> {
                 String email = binding.email.getText().toString();
                 String password = binding.pw.getText().toString();
+                nickname = binding.nickname.getText().toString().trim();
+                String passwordCheck = binding.pwCheck.getText().toString();
+
+                if (!password.equals(passwordCheck)) {
+                    // 비밀번호와 비밀번호 확인이 일치하지 않으면 토스트를 표시하고 회원가입 중단
+                    Toast.makeText(getApplicationContext(), "비밀번호 확인이 일치하지 않습니다.", Toast.LENGTH_SHORT).show();
+                    return; // 메소드 종료
+                }
 
                 signUp(email, password);
         });
@@ -37,6 +52,10 @@ public class SignUp extends AppCompatActivity {
         binding.BackButton.setOnClickListener(v -> {
             finish();
         });
+    }
+
+    private void initFirebaseAuth() {
+        mAuth = FirebaseAuth.getInstance();
     }
 
     private void signUp(String email, String password) {
@@ -49,11 +68,17 @@ public class SignUp extends AppCompatActivity {
                             Log.d("signin", "createUserWithEmail:success");
                             FirebaseUser user = mAuth.getCurrentUser();
 
+                            Map<String, Object> userMap = new HashMap<>();
+                            userMap.put(FirebaseId.documentId, user.getUid());
+                            userMap.put(FirebaseId.email, email);
+                            userMap.put(FirebaseId.password, password);
+                            userMap.put(FirebaseId.nickname, nickname);
+                            userDB.collection(FirebaseId.user).document(user.getUid()).set(userMap, SetOptions.merge());
                             updateUI(user);
                         } else {
                             // If sign in fails, display a message to the user.
                             //Log.w(TAG, "createUserWithEmail:failure", task.getException());
-                            Log.d("signin", "createUserWithEmail:failure");
+                            Log.d("signin", "createUserWithEmail:failure", task.getException());
                             Toast.makeText(getApplicationContext(), "Authentication failed.",
                                     Toast.LENGTH_SHORT).show();
                             updateUI(null);
@@ -64,10 +89,11 @@ public class SignUp extends AppCompatActivity {
 
     private void updateUI(FirebaseUser user) {
         if (user != null) {
-            Intent intent = new Intent(this, MainActivity.class);
+            Intent intent = new Intent(this, Login.class);
             intent.putExtra("USER_PROFILE", "email: " + user.getEmail() + "\n" + "uid: " + user.getUid());
 
             startActivity(intent);
+            finish();
         }
     }
 
