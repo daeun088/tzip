efdd4136745e1eabcfa9141eadc265843f79ab47
minimine09 <lixd289@gmail.com>
2023-11-29 09:32:33 +0900

    원투고 집

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -61,4 +61,5 @@ dependencies {
     // Declare the dependency for the Cloud Firestore library
     // When using the BoM, you don't specify versions in Firebase library dependencies
     implementation("com.google.firebase:firebase-firestore")
+
 }
