58d9fc88e3629b44fb4af22fba2bfda3dda024a8
minimine09 <lixd289@gmail.com>
2023-12-03 22:28:13 +0900

    home list는 아직 구현 x

--- app/src/main/java/com/example/tzip/Fragment_home.java
+++ app/src/main/java/com/example/tzip/Fragment_home.java
@@ -1,42 +1,44 @@
 package com.example.tzip;
 
+import android.content.Context;
+import android.graphics.Rect;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.core.view.ViewCompat;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.LinearLayout;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link Fragment_home#newInstance} factory method to
- * create an instance of this fragment.
- */
+import com.example.tzip.databinding.FragmentCommunityBinding;
+import com.example.tzip.databinding.FragmentHomeBinding;
+import com.example.tzip.databinding.ItemCommunityInnerBinding;
+import com.example.tzip.databinding.ItemHomeCardBinding;
+import com.example.tzip.databinding.ItemHomeListBinding;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Fragment_home extends Fragment {
-
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
+
+    FragmentHomeBinding binding;
+    ItemHomeListBinding binding2;
 
     public Fragment_home() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment Fragment_home.
-     */
-    // TODO: Rename and change types and number of parameters
     public static Fragment_home newInstance(String param1, String param2) {
         Fragment_home fragment = new Fragment_home();
         Bundle args = new Bundle();
@@ -58,7 +60,155 @@ public class Fragment_home extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false);
+
+        binding = FragmentHomeBinding.inflate(inflater, container, false);
+
+        List<String> list = new ArrayList<>();
+        for (int i = 0; i < 20; i++) {
+            list.add("Item=" + i);
+        }
+
+        binding.recyclerviewHomeCard.setLayoutManager(new LinearLayoutManager(requireContext(), RecyclerView.HORIZONTAL, false));
+        binding.recyclerviewHomeCard.setAdapter(new CardAdapter(list));
+        binding.recyclerviewHomeCard.addItemDecoration(new MyItemDecoration());
+
+        return binding.getRoot();
+    }
+
+    private class MyItemDecoration extends RecyclerView.ItemDecoration {
+        @Override
+        public void getItemOffsets(@NonNull Rect outRect, @NonNull View view, @NonNull RecyclerView parent, @NonNull RecyclerView.State state) {
+            int index = parent.getChildAdapterPosition(view) + 1;
+
+            if (index % 3 == 0)
+                outRect.set(20, 20, 20, 60);
+            else
+                outRect.set(20, 20, 20, 20);
+            ViewCompat.setElevation(view, 20.0f);
+        }
+    }
+
+    // 카드 뷰홀더
+
+    private class CardViewHolder extends RecyclerView.ViewHolder {
+        private ItemHomeCardBinding binding;
+
+        private CardViewHolder(ItemHomeCardBinding binding) {
+            super(binding.getRoot());
+            this.binding = binding;
+        }
+    }
+
+    // 카드 뷰홀더
+
+    // 카드 어댑터
+    private class CardAdapter extends RecyclerView.Adapter<CardViewHolder> {
+        private List<String> list;
+
+        private CardAdapter(List<String> list) {
+            this.list = list;
+        }
+
+        @NonNull
+        @Override
+        public CardViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+            ItemHomeCardBinding binding = ItemHomeCardBinding.inflate(LayoutInflater.from(parent.getContext()), parent, false);
+            return new CardViewHolder(binding);
+        }
+
+        @Override
+        public void onBindViewHolder(@NonNull CardViewHolder holder, int position) {
+            String text = list.get(position);
+            holder.binding.homeCardItem.setText(text);
+        }
+
+        @Override
+        public int getItemCount() {
+            return list.size();
+        }
+    }
+    // 카드 어댑터
+
+    // 리스트 어댑터
+    private class ListAdapter extends BaseAdapter {
+
+        List<HomeListItemData> items = null;
+        Context context;
+
+        public ListAdapter(Context context, List<HomeListItemData> items) {
+            this.items = items;
+            this.context = context;
+        }
+
+        @Override
+        public int getCount() {
+            return items.size();
+        }
+
+        @Override
+        public HomeListItemData getItem(int position) {
+            return items.get(position);
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return position;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+
+            if (convertView == null) {
+                LayoutInflater inflater = LayoutInflater.from(context);
+                binding2 = ItemHomeListBinding.inflate(inflater, parent, false);
+                binding2.getRoot().setTag(binding2);
+            } else {
+                binding2 = (ItemHomeListBinding) convertView.getTag();
+            }
+
+            // ListView의 Item을 구성하는 뷰 세팅
+            HomeListItemData item = items.get(position);
+            binding2.homeListImage.setImageResource(item.getImage());
+            binding2.homeListName.setText(item.getName());
+            binding2.homeListTitle.setText(item.getTitle());
+            binding2.homeListLocation.setText(item.getLocation());
+
+            // 설정한 binding.getRoot()를 반환해줘야 함
+            return binding2.getRoot();
+
+        }
+
+        // 리스트 어댑터
+
+        private class HomeListItemData {
+            private int image;
+            private String name;
+            private String title;
+            private String location;
+
+            // 생성자 함수
+            public HomeListItemData(int image, String name, String title, String location) {
+                this.image = image;
+                this.name = name;
+                this.title = title;
+                this.location = location;
+            }
+
+            public int getImage() {
+                return image;
+            }
+
+            public String getName() {
+                return name;
+            }
+
+            public String getTitle() {
+                return title;
+            }
+
+            public String getLocation() {
+                return location;
+            }
+        }
     }
 }
