4995e4b3fd039c1ecf682be6b607bbb2822dfdee
daeun088 <kde2329@naver.com>
2023-12-11 05:45:57 +0900

    onClick 조금 수정!!!

--- app/src/main/java/com/example/tzip/FriendTripRecord.java
+++ app/src/main/java/com/example/tzip/FriendTripRecord.java
@@ -8,11 +8,12 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link FriendTripRecord#newInstance} factory method to
- * create an instance of this fragment.
- */
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class FriendTripRecord extends Fragment {
 
     // TODO: Rename parameter arguments, choose names that match
@@ -28,15 +29,6 @@ public class FriendTripRecord extends Fragment {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment FriendTripRecord.
-     */
-    // TODO: Rename and change types and number of parameters
     public static FriendTripRecord newInstance(String param1, String param2) {
         FriendTripRecord fragment = new FriendTripRecord();
         Bundle args = new Bundle();
@@ -60,5 +52,45 @@ public class FriendTripRecord extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_friend_trip_record, container, false);
+    }
+
+    private void retrieveFriendIds() {
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+        String currentUserId = FirebaseAuth.getInstance().getCurrentUser().getUid();
+
+        // Firestore에서 friendIds 가져오기
+        db.collection("friends").document(currentUserId).get()
+                .addOnSuccessListener(documentSnapshot -> {
+                    if (documentSnapshot.exists()) {
+                        List<String> friendIds = (List<String>) documentSnapshot.get("friendIds");
+                        if (friendIds != null) {
+                            // friendIds를 사용하여 사용자 정보 조회
+                            retrieveUserInformation(friendIds);
+                        }
+                    }
+                })
+                .addOnFailureListener(e -> {
+                    //에러처리
+                });
+    }
+
+    private void retrieveUserInformation(List<String> friendIds) {
+        List<String> friendNames = new ArrayList<>();
+
+        // friendIds에 해당하는 사용자 정보를 조회하고 friendNames에 추가
+        for (String friendId : friendIds) {
+            FirebaseFirestore.getInstance().collection("user").document(friendId).get()
+                    .addOnSuccessListener(documentSnapshot -> {
+                        if (documentSnapshot.exists()) {
+                            String friendName = documentSnapshot.getString("nickname");
+                            friendNames.add(friendName);
+
+                            // friendNames가 완성
+                        }
+                    })
+                    .addOnFailureListener(e -> {
+                        // 오류 처리
+                    });
+        }
     }
 }
